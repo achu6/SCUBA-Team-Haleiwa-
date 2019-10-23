d5497374d90fef6916f323df73b2b75ffb94936c
Andy <achu6@hawaii.edu>
2019-10-22 22:45:52 -1000

    Input depth for maximum time limit at that depth

---
+++ source.py
@@ -0,0 +1,4 @@
+depth = input("Enter desired depth in meters (1 - 42):")
+maxTime = [219, 219 , 219 ,219, 219, 219 , 219 ,219, 219, 219 , 147, 147, 98, 98, 72, 72, 56, 56, 45, 45, 37, 37, 29, 29,29,20,20,20,20,20,14,14,14,14,14,9,9,9,9,9,8,8]
+print("Max time at", depth, "meters is: ", maxTime[int(depth) -1], "minutes")
+
